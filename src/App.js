ecae3baa510794a711a4b6c19279dd91c1616519
Lydia <somanwai@hotmail.com.hk>
2022-05-26 14:48:12 +0800

    router set

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Home from './pages/Layout'
+import Login from './pages/Login'
+
 function App() {
-  return <div className="App">App根組件</div>
+  return (
+    <Router>
+      <div className="App">
+        <Link to="/login">登錄</Link>
+        <Link to="/home">首頁</Link>
+
+        <Switch>
+          <Route path="/home" component={Home}></Route>
+          <Route path="/login" component={Login}></Route>
+          {/* 今晚自己在這裡配置404 */}
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/pages/Layout/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class Layout extends Component {
+  render() {
+    return <div>首頁佈局組件</div>
+  }
+}
